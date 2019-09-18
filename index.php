2f7d02e1bbaf9a47161b53f459d1e4227e295b84
Alexandre Cintra <alrcintra@gmail.com>
2019-09-18 11:46:24 -0300

    Reinício. Novo index

--- index.php
+++ index.php
@@ -1,43 +1,19 @@
-<?php REQUIRE 'head.php'; ?>
-<?php #REQUIRE 'action_dlt.php'; ?>
+<!DOCTYPE html>
+<html>
 
-<script type="text/javascript">
-	
-document.getElementById('imediato').onclick = function() {
-   document.write(1 + 1);
-}​;​
+<head>
+<meta charset="UTF-8">
+<title> Tarefa de Delay Discounting </title>
+</head>
 
+<body>
 
-
-</script>
-
-
-<!-- content here -->
-
-<div class="container">
-	<div class="row justify-content-center">
-	    <div class="col-md-5 text-center">	    	
-		    <h3>Você prefere receber</h3> <br><br><br>	    
-	    	<form method="POST" id="dlcMonetario">
-		    	
-		    	<button type="submit" id="imediato" name ="choice" class="btn btn-primary btn-lg" value="500">R$500,00<br>Agora</button>
-		    	<span> ou </span>    		
-		    	<button type="submit" id="atrasado" name ="choice" class="btn btn-primary btn-lg" value="1000">R$1000,00<br>Em 1 Semana</button>
-		    	<p id="teste"></p>
-
-	    	</form>
- 	    </div>
-	</div>
-</div>
-
-<!-- content here/end -->
-<?php REQUIRE 'footer.php'; ?>
-   
+<form>
+	<input type="button" id="btn1" name="btn1" value="">
+	<input type="button" id="btn2" name="btn2" value="">
 
 
+</form>
 
-
-
-
-
-    
+</body>
+</html>
