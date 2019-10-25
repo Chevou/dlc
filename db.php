d9137bb422b8cee8c91fabd58e12de5fcc7972d8
Alexandre Cintra <alrcintra@gmail.com>
2019-10-25 15:55:44 -0300

    DB_set function configurada, criada variáveis com definições mysql para tabelas

--- db.php
+++ db.php
@@ -29,16 +29,38 @@ function db_set () {
 	$sql = "CREATE DATABASE IF NOT EXISTS dlcr";
 	mysqli_query(db_connect(), $sql);
 
-	$tabinfo;
-	$tabigd;
-	$tabddm;
-	$tabddg;
+	$tb_user = "CREATE TABLE IF NOT EXISTS user (
+		user_id INT AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR NOT NULL, /*PROVISÓRIO */
+		sub_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ) "; 
+
+	$tb_tempo_tarefa = "CREATE TABLE IF NOT EXISTS tempo_tarefa (
+		tempo_id INT AUTO_INCREMENT PRIMARY KEY,
+		tempo VARCHAR(50) NOT NULL,
+		tarefa VARCHAR(50) NOT NULL )";
+
+	$tb_results = "CREATE TABLE IF NOT EXISTS results (
+		result_id INT AUTO_INCREMENT PRIMARY KEY,
+		user_id INT FOREIGN KEY REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+		tempo_id INT FOREIGN KEY REFERENCES tempo_tarefa(tempo_id) ON UPDATE CASCADE ON DELETE CASCADE,
+		r1 INT DEFAULT 0 NOT NULL,
+		r2 INT DEFAULT 0 NOT NULL,
+		r3 INT DEFAULT 0 NOT NULL,
+		r4 INT DEFAULT 0 NOT NULL,
+		r5 INT DEFAULT 0 NOT NULL,
+		r6 INT DEFAULT 0 NOT NULL ) ";
+
+	
 }
 
 
 function insert () {
 	// insert data into the db 
 	// prepared statments	
+}
+
+function update () {
+	// update result rows
 }
 
 db_set();
